b52c2c0b9b2183109eebd60df52a00dc33cfe6fc
KattaSarathTeja <36980176+KattaSarathTeja@users.noreply.github.com>
2020-01-10 05:17:03 +0530

    ADDING FILES

---
+++ build/precache-manifest.96a549783272eb53b88099ac069001a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5306b69bf1c16151cc140556752622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09bc4a51c45092bbbfda",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9c823fe8826e39a4360b",
+    "url": "/static/css/main.9010554e.chunk.css"
+  },
+  {
+    "revision": "09bc4a51c45092bbbfda",
+    "url": "/static/js/2.8bace378.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8bace378.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c823fe8826e39a4360b",
+    "url": "/static/js/main.81809f1a.chunk.js"
+  },
+  {
+    "revision": "01cfe46d64e7031e80ea",
+    "url": "/static/js/runtime-main.0a3b50ab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
