86a5ab17ebe37ad27a761c1ade088ffcf02fbbe6
KattaSarathTeja <36980176+KattaSarathTeja@users.noreply.github.com>
2020-01-10 05:43:21 +0530

    change in package.json

---
+++ build/precache-manifest.0128ebc2aebf213cc741f5fff0cdee3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebce21867f43997b11c8012880061593",
+    "url": "/KattaSarathTeja.robofriends/index.html"
+  },
+  {
+    "revision": "3b73c8d24a45c7e2626e",
+    "url": "/KattaSarathTeja.robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8c96edd40771b84d40f1",
+    "url": "/KattaSarathTeja.robofriends/static/css/main.6c865a88.chunk.css"
+  },
+  {
+    "revision": "3b73c8d24a45c7e2626e",
+    "url": "/KattaSarathTeja.robofriends/static/js/2.c13d78d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/KattaSarathTeja.robofriends/static/js/2.c13d78d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c96edd40771b84d40f1",
+    "url": "/KattaSarathTeja.robofriends/static/js/main.a0c8b749.chunk.js"
+  },
+  {
+    "revision": "d14191a10ceaab87816e",
+    "url": "/KattaSarathTeja.robofriends/static/js/runtime-main.9733212e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/KattaSarathTeja.robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96a549783272eb53b88099ac069001a4.js"
+  "/KattaSarathTeja.robofriends/precache-manifest.0128ebc2aebf213cc741f5fff0cdee3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KattaSarathTeja.robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
